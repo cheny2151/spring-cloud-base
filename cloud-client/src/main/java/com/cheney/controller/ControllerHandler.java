e207c699e045094523c77a0de2ab885650127c5c
cheny2151 <cheny21512151@gmail.com>
2018-07-04 23:55:31 +0800

    update feign

--- cloud-client/src/main/java/com/cheney/controller/ControllerHandler.java
+++ cloud-client/src/main/java/com/cheney/controller/ControllerHandler.java
@@ -1,11 +1,15 @@
 package com.cheney.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class ControllerHandler {
+
+    @Value("${profile}")
+    private String profile;
 
     @ExceptionHandler
     @ResponseBody
